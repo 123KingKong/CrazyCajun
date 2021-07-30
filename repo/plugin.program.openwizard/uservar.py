b69d317a018b9879f59961dd4a604957ba2e128c
Patrick <therealrusha92@gmail.com>
2021-07-29 19:46:33 -0500

    build

--- repo/plugin.program.openwizard/uservar.py
+++ repo/plugin.program.openwizard/uservar.py
@@ -17,7 +17,7 @@ ADDONTITLE = '[COLOR limegreen][B]Open[/B][/COLOR]Wizard'
 BUILDERNAME = 'OpenWizard'
 EXCLUDES = [ADDON_ID, 'repository.openwizard']
 # Text File with build info in it. Please read https://github.com/a4k-openproject/plugin.program.openwizard/wiki/Installing-Builds
-BUILDFILE = 'http://'
+BUILDFILE = 'https://github.com/123KingKong/CrazyCajun/blob/master/repo/plugin.program.openwizard/resources/text/builds.txt'
 # How often you would like it to check for build updates in days
 # 0 being every startup of kodi
 UPDATECHECK = 0
