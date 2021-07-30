935dbfcdceebc3cbf83e8e4054116e748201119b
Patrick <therealrusha92@gmail.com>
2021-07-30 08:00:29 -0500

    wiz

--- repo/plugin.program.CrazyCajunWizard/addonvar.py
+++ repo/plugin.program.CrazyCajunWizard/addonvar.py
@@ -5,7 +5,7 @@ from datetime import datetime
 addon_id = xbmcaddon.Addon().getAddonInfo('id')
 
 '''#####-----Build File-----#####'''
-buildfile = 'https://github.com/123KingKong/CrazyCajun/blob/master/repo/plugin.program.slam19wizard/resources/texts/builds.xml'
+buildfile = 'http://CHANGEME'
 
 '''#####-----Notifications File-----#####'''
 notify_url  = 'http://slamiousproject.com/wzrd/notify19.txt'
